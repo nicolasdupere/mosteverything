5b4625e16f54ba808b60e08d52b9385e11109744
Nicolas Dupere <nico@nico-world.com>
2018-04-03 17:34:43 -0400

    problem3

--- decode/02_test-cases-practice/problems/problem3.js
+++ decode/02_test-cases-practice/problems/problem3.js
@@ -3,23 +3,36 @@ var assert = require('assert');
 // we need 7 test cases. I've provided 2.
 let inputs = [
   [2, 4],
-  [-3, 3]
+  [-3, 3],
+  [6, 4],
+  [2, 9],
+  [5, 7],
+  [-2, -4],
+  [-2, 4]
 ]
 
 let outputs = [
-  6,
-  0
+  6, 0, 10, 11, 12, -6, 2
 ]
 
 /*
 Make this function return the sum of the two numbers that are passed to it. If one of the numbers is not passed, or if anything other than numbers are passed, return undefined.
 */
-function f(x, y) {
-    
+function f(arr) { // [2, 4]
+  if (typeof arr[0] !== "number" || typeof arr[1] !== "number" ) {
+    console.log("Not a number");
+  }
+  //console.log(typeof arr);
+  console.log("Nombre", arr);
+  const add = (a, b) => a + b
+  const sum = arr.reduce(add);
+  return sum;
+  
 }
 
 function runTest(i) {
     var expected = outputs[i];
+    console.log("Expect", expected);
     var actual = f(inputs[i]);
     assert.deepEqual(actual, expected);
 }
@@ -31,3 +44,5 @@ runTest(3);
 runTest(4);
 runTest(5);
 runTest(6);
+console.log("test cases passed");
+
