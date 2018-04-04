ec9c8ddd6371ca10977f18ffb25254718cd8f7fc
Nicolas Dupere <nico@nico-world.com>
2018-04-04 16:28:11 -0400

    problem1

--- decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
+++ decode/03_javascript-fundamentals/01_expansion/paperproblems/problem1.js
@@ -19,10 +19,40 @@ function reverseString(str) {
 }
 // Expand each of the following and get the result of the expression
 // #1
-square(decrement(square(decrement(3))))
+console.log(square(decrement(square(decrement(3)))));
+
+var a = decrement(3);
+var b = square(a);
+var c = decrement(b);
+var d = square(c);
+
+console.log(d);
+
 // #2
-decrement(decrement(square(square(3))))
+console.log(decrement(decrement(square(square(3)))));
+
+var e = square(3);
+var f = square(e);
+var g = decrement(f);
+var h = decrement(g);
+
+console.log(h);
 // #3
-duplicateString(reverseString("hello"))
+console.log(duplicateString(reverseString("hello")));
+
+var i = reverseString("hello");
+var j = duplicateString(i);
+
+console.log(j);
+
+
 // #4
-reverseString(duplicateString(duplicateString("foo")))
+console.log(reverseString(duplicateString(duplicateString("foo"))));
+
+var k = duplicateString("foo");
+var l = duplicateString(k);
+var m = reverseString(l);
+
+console.log(m);
+
+
