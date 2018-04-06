edb3c80689e9239c566f26b5122f3a9b5f386a8d
Nicolas Dupere <nico@nico-world.com>
2018-04-06 15:24:08 -0400

    return1HOF/exceptions

--- decode/03_javascript-fundamentals/04_HOF/src/problem1.js
+++ decode/03_javascript-fundamentals/04_HOF/src/problem1.js
@@ -1,31 +1,59 @@
 
 function callNoException(f, arg) {
-    // if f(arg) throws an exception, return null
-    // otherwise return what f(arg) returned
-    // Example:
-    //  function throwsZero(x){
-    //     if(x==0) throw new Error("woops");
-    //     return x;
-    //  }
-    //  callNoException(throwsZero, 0) returns null
-    //  callNoException(throwsZero, 12) returns 12
-}
+ 
+    try {
+       f(arg)
+    } catch(err) {
+ 
+      return null;
+      }
+   return arg;
+ }
+ 
+ function throwsZero(x){
+         if(x==0) throw new Error("woops");
+         return x;
+   
+      }
+ 
+//  console.log(callNoException(throwsZero, 0)) //returns null
+//  console.log(callNoException(throwsZero, 12)) //returns 12
+
+
+
+ // if f(arg) returns null, throw an exception
+    // otherwise return what f(arg) returned  
+   
 
 function callNoNull(f, arg) {
-    // if f(arg) returns null, throw an exception
-    // otherwise return what f(arg) returned  
-    // Example: 
-    //  function nullZero(x) {
-    //     if(x==0) return null;
-    //     return x;
-    //  }
-    // callNoNull(nullZero, 0) throws an exception
-    // callNoNull(nullZero, 12) returns 12
-    
-    
+     
+    if(f(arg) === null) { 
+      throw new Error("woops");
+      
+    }else {
+      return f(arg);
+    }
+  
 }
 
+function nullZero(x) {
+     if(x==0) return null;
+      return x;
+   } 
+
+// console.log(callNoNull(nullZero, 0)) // throws an exception
+// console.log(callNoNull(nullZero, 12)) // returns 12
+
+
+
 function exceptionalize(f) {
+
+    return function a (arg) {
+
+       if (f(arg) === null) throw new Error ("Error");
+       return f(arg);
+    }
+    
     // returns a new function
     // this function takes 1 input, called arg
     // if f(arg) is null, this new function throws an exception
@@ -42,7 +70,21 @@ function exceptionalize(f) {
 
 }
 
+
+
+
 function nullify(f) {
+
+    function ret (arg) {
+        try {
+            return f(arg) 
+        } catch (err) {
+            return null;
+        }
+        
+    }
+
+    return ret
     // returns a new function
     // this function takes 1 input, called arg
     // if f(arg) throws an exception, this new function returns null
@@ -69,11 +111,20 @@ function map(lst, f) {
     //
     // function toUpperCase(str) { return str.toUpperCase(); }
     // map(["bob", "susie"], toUpperCase) returns ["BOB", "SUSIE"]
+
+    var ret [];
+
+    for (var i = 0; i < lst.length; i++) {
+        ret[i] = f(lst[i]);
+    }
+    return ret;
 }
+
+
 
 function filter(lst, f) {
     // lst is an array and f is a function
-    // f takes one argument and returns a boolean (true or false)
+    // f takes one argument and returns an array
     // filter(lst, f) returns a list with all the elements of lst that does not satisfy f removed
     // filter(lst, f) has fewer elements than lst
     // if lst_ = filter(lst, f) and x is an element of lst_ it means that:
@@ -82,8 +133,17 @@ function filter(lst, f) {
     //   
     // Example:
     // function isEven(x) {return x % 2 == 0;}
-    // filter([1, 2, 3, 4, 5], isEven) returns [2,4];   
-}
+    // filter([1, 2, 3, 4, 5], isEven) returns [2,4];  
+    var ret = [];
+    for (var i = 0; i < lst.length; i++) {
+        if (f(lst[i])) { ret.push(lst[1]); }
+        }
+        return ret; 
+    }
+    function isOdd(x) {
+        return x % 2 === 1;
+    }
+filter([3,1,5,2,2,9], isOdd);
 
 function every(lst, f) {
     // lst is an array and f is a function
