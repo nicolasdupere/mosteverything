053f677de284cfa05f868b8a67aa5241e499ca69
Nicolas Dupere <nico@nico-world.com>
2018-04-09 10:33:04 -0400

    problem1

--- decode/03_javascript-fundamentals/08_this/src/problem1.js
+++ decode/03_javascript-fundamentals/08_this/src/problem1.js
@@ -1,6 +1,10 @@
 'use strict';
 function whatsMyAgeAgain() {
     // returns this.age unless this is not defined. If this is not defined, return 18
+   if(this !== undefined) {
+    return this.age
+   }
+    return 18;
 }
 
 module.exports = whatsMyAgeAgain;
