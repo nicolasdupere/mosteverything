b84ecc4488ce8f74a0a467e5b5d96ee40ab4e3e4
Nicolas Dupere <nico@nico-world.com>
2018-04-13 12:37:15 -0400

    problem 1 2 3 4

--- decode/03_javascript-fundamentals/09_bind/src/problem1.js
+++ decode/03_javascript-fundamentals/09_bind/src/problem1.js
@@ -1,7 +1,12 @@
-var bob = {name: "Bob"};
+var patate = {
+    name: "Bob"
+};
+
 function greet() {
     return "I'm " + this.name;
 }
+
+var greet = greet.bind(patate);
 // bind greet to bob
 
 module.exports = greet;
--- decode/03_javascript-fundamentals/10_variable-scoping/src/problem1.js
+++ decode/03_javascript-fundamentals/10_variable-scoping/src/problem1.js
@@ -3,8 +3,16 @@
 // The first time it is called it returns 1
 // Every call thereafter returns a number one greater than the last
 
+// function f() {
+//     var x = 0;  
+//     x = x + 1;
+//     return x;
+// }
+
+var x = 0; 
+
 function f() {
-    var x = 0;  
+     
     x = x + 1;
     return x;
 }
